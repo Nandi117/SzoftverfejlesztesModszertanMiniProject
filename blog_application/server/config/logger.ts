a89713c022db75ad7882e1fcb5d127a92d3021c7
Molnár Bálint <balint20010113@gmail.com>
2024-09-28 16:56:59 +0200

    feat: Add Winston logger to the server for logging

---
+++ blog_application/server/config/logger.ts
@@ -0,0 +1,12 @@
+import {createLogger, transports, format} from "winston";
+
+export const logger = createLogger({
+
+    level: 'info',
+    format: format.json(),
+    transports: [
+        new transports.Console(),
+        /*new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' }), Fájlba logolás*/
+    ],
+});
