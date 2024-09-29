f36d96547345d097cc6440eaca3bd53a54ed9777
matyivagyok <150827933+matyivagyok@users.noreply.github.com>
2024-09-29 15:27:15 +0300

    feat: Update LogInPage layout and styling

--- blog_application/frontend/src/pages/LogInPage/LogInPage.tsx
+++ blog_application/frontend/src/pages/LogInPage/LogInPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, FormControl, FormLabel, Input, Heading, VStack, Alert, AlertIcon } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Input, Heading, VStack, Alert, AlertIcon, Flex } from '@chakra-ui/react';
 import { UnlockIcon } from '@chakra-ui/icons';
 
 const LogInPage = () => {
@@ -17,38 +17,40 @@ const LogInPage = () => {
     };
 
     return (
-        <Box width="100%" maxWidth="400px" mx="auto" mt="10">
-            <VStack spacing={4} align="stretch">
-                <Heading as="h1" size="lg" textAlign="center">
-                    Log In
-                </Heading>
-                {error && (
-                    <Alert status="error">
-                        <AlertIcon />
-                        {error}
-                    </Alert>
-                )}
-                <FormControl id="email">
-                    <FormLabel>Email address</FormLabel>
-                    <Input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </FormControl>
-                <FormControl id="password">
-                    <FormLabel>Password</FormLabel>
-                    <Input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </FormControl>
-                <Button leftIcon={<UnlockIcon/>} colorScheme="teal" onClick={handleLogin}>
-                    Log In
-                </Button>
-            </VStack>
-        </Box>
+        <Flex height={"100vh"} alignItems={"center"} justifyContent={"center"}>
+            <Box width="100%" maxWidth="400px" p="8" borderWidth="2px" borderRadius="30px" boxShadow="x1" borderColor="gray.200">
+                <VStack spacing={4} align="stretch">
+                    <Heading as="h1" size="lg" textAlign="center">
+                        Log In
+                    </Heading>
+                    {error && (
+                        <Alert status="error">
+                            <AlertIcon />
+                            {error}
+                        </Alert>
+                    )}
+                    <FormControl id="email">
+                        <FormLabel>Email address</FormLabel>
+                        <Input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl id="password">
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+                    <Button leftIcon={<UnlockIcon/>} colorScheme="teal" onClick={handleLogin}>
+                        Log In
+                    </Button>
+                </VStack>
+            </Box>
+        </Flex>
     );
 };
 
