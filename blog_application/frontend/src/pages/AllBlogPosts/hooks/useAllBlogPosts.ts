5d9d5af4da530db0edad6e318346b652e18afc36
Szabó Zsombor <77261517+SzbZsmbr@users.noreply.github.com>
2024-09-29 15:05:43 +0300

    Refactor import statement in useAllBlogPosts.ts

--- blog_application/frontend/src/pages/AllBlogPosts/hooks/useAllBlogPosts.ts
+++ blog_application/frontend/src/pages/AllBlogPosts/hooks/useAllBlogPosts.ts
@@ -1,19 +1,19 @@
-import { useCallback, useEffect, useState } from "react";
-import { getApi } from "../../../config/api.ts";
-import { useDispatch, useSelector } from "react-redux";
-import { setAllPosts } from "../../../store/allPosts/allPostsSlice.ts";
+import {useCallback, useEffect, useState} from "react";
+import {getApi} from "../../../config/api.ts";
+import {useDispatch, useSelector} from "react-redux";
+import {setAllPosts} from "../../../store/allPosts/allPostsSlice.ts";
 
 export const useAllBlogPosts = () => {
     const [loading, setLoading] = useState<boolean>(true);
     const dispatch = useDispatch();
-    const posts = useSelector((state: RootState) => state.allPosts.posts);
+    const posts = useSelector(state => state.allPosts.posts);
 
     const getAllPosts = useCallback(async () => {
         try {
             const response = await getApi().get("blogs");
             dispatch(setAllPosts(response.data));
         } catch (e) {
-            // Handle error
+            console.error(e);
         } finally {
             setLoading(false);
         }
