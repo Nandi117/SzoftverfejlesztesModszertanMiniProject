9ab4bcb9bacb546b974fba56908e5bdd9308236f
Drevenyák Bálint <drevenyak.balint>
2024-09-29 16:23:01 +0200

    added register form in SignUpPage.tsx

--- blog_application/frontend/src/pages/SignUpPage/SignUpPage.tsx
+++ blog_application/frontend/src/pages/SignUpPage/SignUpPage.tsx
@@ -19,6 +19,59 @@ const SignUpPage = () => {
             console.log('Signing up with', username, email, password);
         }
     };
+
+    return (
+        <Flex height={"100vh"} alignItems={"center"} justifyContent={"center"}>
+            <Box width="100%" maxWidth="400px" p="8" borderWidth="2px" borderRadius="30px" boxShadow="x1" borderColor="gray.200">
+                <VStack spacing={4} align="stretch">
+                    <Heading as="h1" size="lg" textAlign="center">
+                        Sign Up
+                    </Heading>
+                    {error && (
+                        <Alert status="error">
+                            <AlertIcon />
+                            {error}
+                        </Alert>
+                    )}
+                    <FormControl id="username">
+                        <FormLabel>Username</FormLabel>
+                        <Input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl id="email">
+                        <FormLabel>Email address</FormLabel>
+                        <Input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl id="password">
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl id="confirm-password">
+                        <FormLabel>Confirm Password</FormLabel>
+                        <Input
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </FormControl>
+                    <Button leftIcon={<LockIcon />} colorScheme="teal" onClick={handleSignUp}>
+                        Sign Up
+                    </Button>
+                </VStack>
+            </Box>
+        </Flex>
+    );
 };
 
 export default SignUpPage;
