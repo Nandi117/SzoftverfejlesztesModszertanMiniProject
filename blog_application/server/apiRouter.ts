73d506775302709d1cb36e2c9a02e1d91776b1e4
Molnár Bálint <balint20010113@gmail.com>
2024-11-16 22:11:14 +0100

    feat: register activityController into apiRouter.ts

--- blog_application/server/apiRouter.ts
+++ blog_application/server/apiRouter.ts
@@ -6,6 +6,7 @@ import blogController from "./controllers/blogController";
 import commentsController from "./controllers/commentsController";
 import testController from "./controllers/testController";
 import authRoutes from "./controllers/authController";
+import activityController from "./controllers/activityController";
 
 const router = Router();
 
@@ -14,6 +15,6 @@ router.use("/blogs", blogController);
 router.use("/blogs/comments", commentsController);
 router.use("/test", testController)
 router.use("/auth", authRoutes)
-
+router.use("/activities", activityController)
 
 export default router;
