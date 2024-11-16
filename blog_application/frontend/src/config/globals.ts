0044d8f14c43531f1dbb45d2735598cdc9c1559e
Molnár Bálint <balint20010113@gmail.com>
2024-11-16 12:20:10 +0100

    config: Add translator-microservice API url to globals.ts

--- blog_application/frontend/src/config/globals.ts
+++ blog_application/frontend/src/config/globals.ts
@@ -3,3 +3,5 @@ import * as process from "process";
 
 
 export const BASE_API_URL:string | undefined = import.meta.env.VITE_API_URL || "";
+
+export const TRANSLATOR_API_URL:string | undefined = import.meta.env.VITE_TRANSLATOR_API_URL || ""
