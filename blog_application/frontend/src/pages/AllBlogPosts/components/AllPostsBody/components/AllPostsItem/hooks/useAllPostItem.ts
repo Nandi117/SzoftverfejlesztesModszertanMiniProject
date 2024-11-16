25f81da500332db076dbc481f05613394e64e92f
Molnár Bálint <balint20010113@gmail.com>
2024-11-16 22:12:33 +0100

    refactor: AllPostType type refactor

--- blog_application/frontend/src/pages/AllBlogPosts/components/AllPostsBody/components/AllPostsItem/hooks/useAllPostItem.ts
+++ blog_application/frontend/src/pages/AllBlogPosts/components/AllPostsBody/components/AllPostsItem/hooks/useAllPostItem.ts
@@ -1,37 +1,16 @@
-import { useCallback, useState } from "react";
-import { getApi } from "../../../../../../../config/api.ts";
-import { useDispatch } from "react-redux";
-import { deletePost } from "../../../../../../../store/allPosts/allPostsSlice.ts";
+
+import {useDispatch} from "react-redux";
 
 export const useAllPostItem = () => {
-    const [delInProgress, setDelInProgress] = useState<boolean>(false);
     const dispatch = useDispatch();
 
     const viewPost = (id: string) => window.open(`posts/${id}`);
 
-    const updateAllPost = useCallback(() => {
-        // Implement update logic here
-    }, []);
 
-    const deleteAllPost = useCallback(async (id: string) => {
-        setDelInProgress(true);
-        try {
-            const response = await getApi().delete(`blogs/${id}`);
-            if (response.status === 200) {
-                const deletedPostId = response.data;
-                dispatch(deletePost(deletedPostId));
-            }
-        } catch (e) {
-            // Handle error
-        } finally {
-            setDelInProgress(false);
-        }
-    }, [dispatch]);
+
+
 
     return {
-        deleteAllPost,
-        updateAllPost,
         viewPost,
-        delInProgress,
     };
 };
