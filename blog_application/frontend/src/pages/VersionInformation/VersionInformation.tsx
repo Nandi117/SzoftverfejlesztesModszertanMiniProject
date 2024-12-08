555301d4fb9210d9f557bec3825d9e792e1d4820
Molnár Bálint <balint20010113@gmail.com>
2024-12-08 23:30:42 +0100

    feat: Add v1.0.2 information to VersionInformation.tsx

--- blog_application/frontend/src/pages/VersionInformation/VersionInformation.tsx
+++ blog_application/frontend/src/pages/VersionInformation/VersionInformation.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Text, List, ListItem, Center } from "@chakra-ui/react";
 
 const VersionInformation = () => {
     return (
-        <Center>
+        <Center mt={"5vh"}>
             <Box p={5} maxW="600px" textAlign="center">
                 <Heading as="h1" mb={5}>Version Information</Heading>
                 <Text mb={3}><strong>Version:</strong> 1.0.0</Text>
@@ -15,7 +15,35 @@ const VersionInformation = () => {
                     <ListItem>Search Functionality: Search posts based on keywords.</ListItem>
                     <ListItem>Version Information Page: Display version and feature information.</ListItem>
                 </List>
+
+                <Box mt={"2em"}>
+                    <Text mb={3}><strong>Version:</strong> 1.0.1</Text>
+                    <Text mb={3}><strong>Features:</strong></Text>
+                    <List spacing={3} textAlign="left">
+                        <ListItem>Activity: Display the current user activity (only for blog post management).</ListItem>
+                        <ListItem>New blog post: Users can add background templates to their posts.</ListItem>
+                        <ListItem>Translation: Users can translate blog posts to hungarian language.</ListItem>
+                        <ListItem>Password changing: Users can change their passwords in the user profile section.</ListItem>
+                        <ListItem>Version Information Page: Display version and feature information v1.0.1.</ListItem>
+                    </List>
+                </Box>
+
+
+                <Box mt={"2em"}>
+                    <Text mb={3}><strong>Version:</strong> 1.0.2</Text>
+                    <Text mb={3}><strong>Features:</strong></Text>
+                    <List spacing={3} textAlign="left">
+                        <ListItem>Activity: Display the current user activity (commenting activity as well).</ListItem>
+                        <ListItem>Accounts page: Users can see all registered accounts.</ListItem>
+                        <ListItem>Updated UI: More attractive UI enabled.</ListItem>
+                        <ListItem>Automated tests: Test automatization.</ListItem>
+                        <ListItem>Version Information Page: Display version and feature information v1.0.2.</ListItem>
+                    </List>
+                </Box>
+
             </Box>
+
+
         </Center>
     );
 };
