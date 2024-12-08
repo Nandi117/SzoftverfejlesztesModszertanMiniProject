2f77495a34703358c59f5b685b59eabfd15bdeca
Molnár Bálint <balint20010113@gmail.com>
2024-12-08 23:14:10 +0100

    feat: Add Logo to Menu component and a little description

--- blog_application/frontend/src/components/Menu/Menu.tsx
+++ blog_application/frontend/src/components/Menu/Menu.tsx
@@ -1,8 +1,9 @@
-import {Box, Button, Flex, Spacer} from "@chakra-ui/react";
+import {Box, Button, Flex, Spacer, Text} from "@chakra-ui/react";
 import {menuItems} from "../../config/menu.ts";
 import {useNavigate} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {closeDrawer} from "../../store/drawer/drawer.slice.ts";
+import {Logo} from "../Logo/Logo.tsx";
 
 export const Menu = () => {
     const navigate = useNavigate();
@@ -15,6 +16,11 @@ export const Menu = () => {
 
     return (
         <Flex flexDirection={"column"} width={"100%"} height={"100%"}>
+            <Flex alignItems={"center"} width={"100%"} justifyContent={"space-around"} py={4}>
+                <Logo/>
+                <Text fontSize={14}>Where words find their wings.</Text>
+            </Flex>
+            <hr/>
             {menuItems.map((menuItem) => {
                 return (
                     <Box key={menuItem.id} width={"100%"} pt={2}>
